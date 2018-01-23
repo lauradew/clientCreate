bfaedb99ec73e3bea2de31ddca16f5dd8775c5cd
Laura De Witt <lauramdewitt@gmail.com>
2018-01-23 01:23:52 +0000

    adding reverse exercise

--- callbacks/print_reverse.js
+++ callbacks/print_reverse.js
@@ -1,3 +1,15 @@
-var getHTML = require('../http-functions');
-var printHTML = require('../http-functions');
+var {getHTML, printHTML} = require('../http-functions');
 var https = require('https');
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step6/uppercase.html'
+};
+
+function reverse (html) {
+  var str = html.split('').reverse().join('');
+  console.log(str);
+}
+
+
+getHTML(requestOptions, reverse);
